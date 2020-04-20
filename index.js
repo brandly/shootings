729f37172266b0e4cc35d7b9fbdea528e0544daf
brandly <brandly13@gmail.com>
2020-04-19 23:14:25 -0400

    fetch two lists, fixes #3

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const cheerio = require('cheerio')
 const moment = require('moment')
 const { groupBy, sum } = require('lodash')
 
+const newUrl =
+  'https://en.wikipedia.org/wiki/List_of_school_shootings_in_the_United_States'
+const oldUrl =
+  'https://en.wikipedia.org/wiki/List_of_school_shootings_in_the_United_States_(before_2000)'
+async function fetchShootings() {
+  const older = await fetch(oldUrl).then(res => res.text())
+  const newer = await fetch(newUrl).then(res => res.text())
+  return parse(older).concat(parse(newer))
+}
+
 fetchShootings()
   .then(shootings => {
     const sep = '\t'
@@ -19,34 +29,31 @@ fetchShootings()
   })
   .catch(err => console.error(err))
 
-function needsEscaping (val) {
+function needsEscaping(val) {
   return val.includes('\n') || val.includes('"') || val.includes(',')
 }
 
-function escapeChars (val) {
+function escapeChars(val) {
   return val.replace(/"/g, '""')
 }
 
-function fetchShootings () {
-  return fetch('https://en.wikipedia.org/wiki/List_of_school_shootings_in_the_United_States')
-    .then(res => res.text())
-    .then(body => parse(body))
-}
-
-function parse (body) {
+function parse(body) {
   const $ = cheerio.load(body)
 
   return $('.wikitable > tbody tr')
-    .filter((index, tr) =>
-      // ignore headings
-      $(tr).children().first().text().trim() !== 'Date'
+    .filter(
+      (index, tr) =>
+        // ignore headings
+        $(tr)
+          .children()
+          .first()
+          .text()
+          .trim() !== 'Date'
     )
     .map((index, tr) =>
       $(tr)
         .children()
-        .map((i, td) =>
-          $(td).text()
-        )
+        .map((i, td) => $(td).text())
         .toArray()
         .reduce((res, val, index) => {
           res[indexToColumn(index)] = val
@@ -64,19 +71,16 @@ function parse (body) {
         deaths: parseInt(deaths),
         // it's probably a '?'. only occurs a few times.
         injuries: Number.isNaN(intInjuries) ? 0 : intInjuries,
-        location: (location.includes('!') ? location.split('!')[1] : location).trim(),
+        location: (location.includes('!')
+          ? location.split('!')[1]
+          : location
+        ).trim(),
         description
       }
     })
 }
 
-const columns = [
-  'date',
-  'location',
-  'deaths',
-  'injuries',
-  'description'
-]
-function indexToColumn (i) {
+const columns = ['date', 'location', 'deaths', 'injuries', 'description']
+function indexToColumn(i) {
   return columns[i]
 }
